eb84415c42bf607c5e4a9ecf8f845dd6086d9148
Marti Kecskemeti <martikecskemeti@gmail.com>
2017-06-22 16:49:21 +0200

    models added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,68 @@
 import com.ibm.watson.developer_cloud.http.ServiceCall;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.*;
+import model.Text;
+import model.User;
+import model.Word;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by marti on 2017.06.20..
  */
 public class Main {
 
+    public static void populateDb(EntityManager em) {
+
+
+        User user = new User("Kiki");
+        Text text = new Text(0.89,"I like apples, but hate oranges.");
+        Word firstWord = new Word("apples",0.75,0.55,0.45, 0.65,0.78,0.11);
+        Word secondWord = new Word("oranges",0.75,0.55,0.45, 0.65,0.78,0.11);
+        text.addWords(firstWord);
+        text.addWords(secondWord);
+        user.addTexts(text);
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(user);
+        em.persist(text);
+        em.persist(firstWord);
+        em.persist(secondWord);
+        transaction.commit();
+        System.out.println("stuff saved.");
+
+    }
+
+
     public static void main(String[] args) {
-        AnalysisResults results = Controller.getData();
-        System.out.println(results);
+        //AnalysisResults results = Controller.getData();
+        //System.out.println(results);
         //System.out.println(results.getSentiment().getDocument().getScore());
         //System.out.println(results.getEntities().get(0).getSentiment());
         //System.out.println(results.getEntities().get(0).getEmotion().getJoy());
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("music-gen");
+        EntityManager em = emf.createEntityManager();
+
+        populateDb(em);
+
+//        Student foundStudent1 = em.find(Student.class, 1L);
+//        System.out.println("--Found student #1");
+//        System.out.println("----name----" + foundStudent1.getName());
+//        System.out.println("----address of student----" + foundStudent1.getAddress());
+
+
+        em.close();
+        emf.close();
 
     }
 }
---
+++ src/main/java/model/Text.java
@@ -0,0 +1,84 @@
+package model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by marti on 2017.06.22..
+ */
+
+@Entity
+public class Text {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String content;
+
+    private double sumSentiment;
+
+    @OneToMany(mappedBy = "text")
+    private List<Word> words = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Text(){}
+
+    public Text(double sumSentiment, String content) {
+        this.sumSentiment = sumSentiment;
+        this.content = content;
+    }
+
+    public List<Word> getWords() {
+        return words;
+    }
+
+    public void addWords(Word word) {
+        if(word.getText() != this){
+            word.setText(this);
+        }
+        words.add(word);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getSumSentiment() {
+        return sumSentiment;
+    }
+
+    public void setSumSentiment(double sumSentiment) {
+        this.sumSentiment = sumSentiment;
+    }
+
+    public void setWords(List<Word> words) {
+        this.words = words;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
